735f333295363e28b21d65c35bd4512b09f42d9c
Sourav Saha <sahasavy@gmail.com>
2025-06-24 19:53:38 +0530

    feat: BRN-01: Initial SpringBoot framework commit (#1)

---
+++ src/main/java/com/algo/trading/AlgoTradingAppApplication.java
@@ -0,0 +1,13 @@
+package com.algo.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoTradingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoTradingAppApplication.class, args);
+	}
+
+}
